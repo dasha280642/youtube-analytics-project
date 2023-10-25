0e6bbac9e06248ba5fe8830a67e4f89bc53daf9d
dasha280642 <dasha19982009@rambler.ru>
2023-10-25 13:56:09 +0300

    e

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,11 +1,27 @@
 from src.channel import Channel
-import os
-import json
-
+from src.channel import print_info
 if __name__ == '__main__':
-    moscowpython = Channel("UC-OVMPlMA3-YCIeg4z5z23A")
-    moscowpython.print_info()
+    channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+    channel = Channel(channel_id)
+    channel_info = channel.get_channel_info()
+    print_info(channel_info)
 
+    playlists = channel.get_playlists()
+    print_info(playlists)
+
+    playlist_id = 'PLH-XmS0lSi_zdhYvcwUfv0N88LQRt6UZn'
+    playlist_videos = channel.get_playlist_videos(playlist_id)
+    print(playlist_videos)
+
+    video_id = 'gaoc9MPZ4bw'
+    video_duration = channel.get_video_duration(video_id)
+    print(video_duration)
+
+    video_title, view_count, like_count, comment_count = channel.get_video_stats(video_id)
+    print(video_title)
+    print(view_count)
+    print(like_count)
+    print(comment_count)
     """
 {
   "kind": "youtube#channelListResponse",
--- src/channel.py
+++ src/channel.py
@@ -1,15 +1,65 @@
 
-
-from googleapiclient.discovery import build
 import json
-
 import os
 
-class Channel:
-    def __init__(self, channel_id):
-        self.channel_id = os.getenv('AIzaSyAEGHL40vqEZXrb_WbFZW_M3nRe-LfJ2xY')
-        self.youtube = build('youtube', 'v3', developerKey=self.channel_id)
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
 
-    def print_info(self):
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=True))
+import isodate
+import os
+
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def print_info(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+
+class Channel:
+    def __init__(self, channel_id: str):
+        self.channel_id = channel_id
+
+    def get_channel_info(self):
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
+
+    def get_playlists(self):
+        playlists = youtube.playlists().list(channelId=self.channel_id,
+                                             part='contentDetails,snippet',
+                                             maxResults=50,
+                                             ).execute()
+        return playlists
+
+    def get_playlist_videos(self, playlist_id: str):
+        playlist_videos = youtube.playlistItems().list(playlistId=playlist_id,
+                                                       part='contentDetails',
+                                                       maxResults=50,
+                                                       ).execute()
+        video_ids = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+        return video_ids
+
+    def get_video_duration(self, video_id: str):
+        video_response = youtube.videos().list(part='contentDetails,statistics',
+                                               id=video_id
+                                               ).execute()
+        iso_8601_duration = video_response['items'][0]['contentDetails']['duration']
+        duration = isodate.parse_duration(iso_8601_duration)
+        return duration
+
+    def get_video_stats(self, video_id: str):
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        video_title = video_response['items'][0]['snippet']['title']
+        view_count = video_response['items'][0]['statistics']['viewCount']
+        like_count = video_response['items'][0]['statistics']['likeCount']
+        comment_count = video_response['items'][0]['statistics']['commentCount']
+        return video_title, view_count, like_count, comment_count
+
+
